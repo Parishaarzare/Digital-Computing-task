2eef1d826468d0f499a8301dc1cb8ba36367a918
Parishaarzare <parishaarzare@gmail.com>
2024-12-03 17:55:07 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,68 @@
+var x,y,result
+function b(){
+    document.getElementById("screen").value += "("
+}
+function bk(){
+    document.getElementById("screen").value += ")"
+}
+function input1(){
+    document.getElementById("screen").value += "1"
+}
+function input2(){
+    document.getElementById("screen").value += "2"
+}
+function input3(){
+    document.getElementById("screen").value += "3"
+}
+function input4(){
+    document.getElementById("screen").value += "4"
+}
+function input5(){
+    document.getElementById("screen").value += "5"
+}
+function input6(){
+    document.getElementById("screen").value += "6"
+}
+function input7(){
+    document.getElementById("screen").value += "7"
+}
+function input8(){
+    document.getElementById("screen").value += "8"
+}
+function input9(){
+    document.getElementById("screen").value += "9"
+}
+function dot(){
+    document.getElementById("screen").value += "."
+}
+function sin(){
+    document.getElementById("screen").value = Math.sin(document.getElementById("screen").value)
+}
+function cos(){
+    document.getElementById("screen").value = Math.cos(document.getElementById("screen").value)
+}
+function tan(){
+    document.getElementById("screen").value = Math.tan(document.getElementById("screen").value)
+}
+function zero(){
+    document.getElementById("screen").value += "0"
+}
+function divd(){
+    document.getElementById("screen").value += "/"
+}
+function add(){
+    document.getElementById("screen").value += "+"
+}
+function muti(){
+    document.getElementById("screen").value += "*"
+}
+function per(){
+    document.getElementById("screen").value += "%"
+}
+function equal(){
+    document.getElementById("screen").value = eval(document.getElementById("screen").value)
+}
+
+
+
+
